9597c5ed0126762b342f22de8eaa764d257d3e61
Amanda Fahy <amandafahy@ymail.com>
2024-10-12 23:05:50 +0000

    Complete webhooks setup

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,16 +1,23 @@
-"""Helps prevent payment being made and order not being recorded"""
 from django.http import HttpResponse
+
+from .models import Order, OrderLineItem
+from products.models import Product
+
+import json
+import time
 
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
+
     def __init__(self, request):
         self.request = request
+
     def handle_event(self, event):
         """
         Handle a generic/unknown/unexpected webhook event
         """
         return HttpResponse(
-            content=f'Webhook received: {event["type"]}',
+            content=f'Unhandled webhook received: {event["type"]}',
             status=200)
 
     def handle_payment_intent_succeeded(self, event):
@@ -21,13 +28,16 @@ class StripeWH_Handler:
         pid = intent.id
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
+
         billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
         grand_total = round(intent.charges.data[0].amount / 100, 2)
+
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
+
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -98,6 +108,7 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
+
     def handle_payment_intent_payment_failed(self, event):
         """
         Handle the payment_intent.payment_failed webhook from Stripe
