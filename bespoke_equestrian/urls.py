d0e95a2bf736163365d83a587ff92ed2f3aacfae
Amanda Fahy <amandafahy@ymail.com>
2024-10-16 00:00:13 +0000

    Add 404 and 500 error pages

--- bespoke_equestrian/urls.py
+++ bespoke_equestrian/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404, handler500
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,5 +13,7 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-  
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'bespoke_equestrian.views.handler404'
+handler500 = 'bespoke_equestrian.views.handler500'
