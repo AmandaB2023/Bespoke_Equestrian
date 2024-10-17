33afe70d925cd403c027b7e085cdf3682480248c
Amanda Fahy <amandafahy@ymail.com>
2024-10-17 16:39:38 +0000

    Upadate Css

--- about/models.py
+++ about/models.py
@@ -4,7 +4,6 @@ from cloudinary.models import CloudinaryField
 class About(models.Model):
     title = models.CharField(max_length=200)
     profile_image = CloudinaryField('image', default='placeholder')
-    updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
     def __str__(self):
