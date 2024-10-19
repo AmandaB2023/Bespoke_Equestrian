f024215341865695b7af6a2e753430be6425ebfe
Amanda Fahy <amandafahy@ymail.com>
2024-10-19 16:50:36 +0000

    Update requirements.txt

--- bespoke_equestrian/settings.py
+++ bespoke_equestrian/settings.py
@@ -1,11 +1,6 @@
 
 import os
 import dj_database_url
-import cloudinary
-import django_heroku
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
 
 from pathlib import Path
 if os.path.isfile('env.py'):
@@ -128,19 +123,26 @@ WSGI_APPLICATION = 'bespoke_equestrian.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 else:
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.gitpod.io",
