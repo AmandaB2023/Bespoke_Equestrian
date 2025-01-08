a0d3acfddb7c85fb4cab625f81de3226828e6795
Amanda Fahy <amandafahy@ymail.com>
2025-01-08 03:12:38 +0000

    Change about image to cloudinary

--- about/models.py
+++ about/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 
 class About(models.Model):
     title = models.CharField(max_length=200)
-    about_image = models.FileField(null=True, blank=True)
+    about_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
 
     def __str__(self):
